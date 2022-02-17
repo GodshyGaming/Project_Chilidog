1f740d2c9f06d7061b56664faa0c2a45b3ddb4c7
GodshyGaming <godshygaming@gmail.com>
2022-02-17 10:13:41 -0600

    Added global fields to console program

--- Project_Chilidog/ConsoleEngine/ConsoleProgram.cs
+++ Project_Chilidog/ConsoleEngine/ConsoleProgram.cs
@@ -9,12 +9,39 @@ namespace ConsoleEngine
     /// Entrypoint for console version of program
     /// </summary>
     ///
+    /// <fields>
+    /// _globalSettlements: list of all settlements in the current worldspace
+    /// _globalMerchants: list of all merchants in the current worldspace
+    /// _globalTradeRoutes: list of all trade routes in the current worldspace
+    /// </fields>
+    /// 
     /// <methods>
     /// Main();
     ///     Entrypoint for console version of program
     /// </methods>
     class Program
     {
+        #region Fields
+
+        /// <summary>
+        /// list of all settlements in the current worldspace
+        /// </summary>
+        private List<ISettlement> _globalSettlements = new List<ISettlement>();
+
+        /// <summary>
+        /// list of all merchants in the current worldspace
+        /// </summary>
+        private List<IMerchant> _globalMerchants = new List<IMerchant>();
+
+        /// <summary>
+        /// list of all trade routes in the current worldspace
+        /// </summary>
+        private List<ITradeRoutes> _globalTradeRoutes = new List<ITradeRoutes>();
+
+        #endregion
+
+        #region Methods
+
         /// <summary>
         /// Entrypoint for console version of program
         /// </summary>
@@ -34,5 +61,7 @@ namespace ConsoleEngine
                 Console.WriteLine(command);
             }
         }
+
+        #endregion
     }
 }
