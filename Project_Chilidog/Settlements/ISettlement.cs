c8d6d0d563d75c0a8c16936003e4a9b6b3478d68
GodshyGaming <godshygaming@gmail.com>
2022-02-17 08:45:35 -0600

    Filled out initial ISettlement fields

--- Project_Chilidog/Settlements/ISettlement.cs
+++ Project_Chilidog/Settlements/ISettlement.cs
@@ -5,9 +5,45 @@
     /// </title>
     /// 
     /// <summary>
-    /// determines basic behavior for settlement objects
+    /// Determines basic behavior for settlement objects.
     /// </summary>
+    /// 
+    /// <fields>
+    /// Name: The name of the settlement.
+    /// Description: The description of the settlement.
+    /// Population: Population of the settlement
+    /// LocalTradeRoutes: Trade routes connected to the settlement
+    /// LocalGoods: Goods traded in the settlement
+    /// </fields>
     public interface ISettlement
     {
+        #region Fields
+        
+        /// <summary>
+        /// The name of the settlement.
+        /// </summary>
+        string Name { get; set;  }
+
+        /// <summary>
+        /// The description of the settlement.
+        /// </summary>
+        string Description { get; set; }
+
+        /// <summary>
+        /// Population of the settlement
+        /// </summary>
+        int Population { get; set; }
+
+        /// <summary>
+        /// Trade routes connected to the settlement
+        /// </summary>
+        List<TradeRoutes.ITradeRoutes> LocalTradeRoutes { get; set; }
+
+        /// <summary>
+        /// Goods traded in the settlement
+        /// </summary>
+        List<Goods.IGood> LocalGoods { get; set; }
+
+        #endregion
     }
 }
