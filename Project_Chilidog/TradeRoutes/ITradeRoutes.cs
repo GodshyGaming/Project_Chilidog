8de930fd935e126cc6477e3a9a3373db3a0e2d8a
GodshyGaming <godshygaming@gmail.com>
2022-02-17 09:01:19 -0600

    Created basic fields for ITradeRoutes

--- Project_Chilidog/TradeRoutes/ITradeRoutes.cs
+++ Project_Chilidog/TradeRoutes/ITradeRoutes.cs
@@ -7,7 +7,31 @@
     /// <summary>
     /// determines basic behavior for trade route objects
     /// </summary>
+    /// 
+    /// <fields>
+    /// DestinationA: One end of the trade route, opposite of DestinationB.
+    /// DestinationB: Other end of the trade route, opposite of DestinationA.
+    /// Length: Real distance from DestinationA to DestinationB relative to mode of travel
+    /// </fields>
     public interface ITradeRoutes
     {
+        #region Fields
+
+        /// <summary>
+        /// One end of the trade route, opposite of DestinationB.
+        /// </summary>
+        Settlements.ISettlement DestinationA { get; set; }
+
+        /// <summary>
+        /// Other end of the trade route, opposite of DestinationA.
+        /// </summary>
+        Settlements.ISettlement DestinationB { get; set; }
+
+        /// <summary>
+        /// Real distance from DestinationA to DestinationB relative to mode of travel
+        /// </summary>
+        decimal Length { get; set; }
+
+        #endregion
     }
 }
