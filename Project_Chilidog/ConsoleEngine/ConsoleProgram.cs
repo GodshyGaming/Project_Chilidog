f48c7ca19226c4c665478afb8a0a666698ed44a9
GodshyGaming <godshygaming@gmail.com>
2022-02-17 09:21:33 -0600

    Added ConsoleEngine namespace and moved entrypoint there

---
+++ Project_Chilidog/ConsoleEngine/ConsoleProgram.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleEngine
+{
+    /// <summary>
+    /// Entrypoint for console version of program
+    /// </summary>
+    ///
+    /// <methods>
+    /// Main();
+    ///     Entrypoint for console version of program
+    /// </methods>
+    class Program
+    {
+        /// <summary>
+        /// Entrypoint for console version of program
+        /// </summary>
+        static void Main()
+        {
+
+        }
+    }
+}
