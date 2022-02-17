7e47c3b6af36e45b26eed7abc455d38575acd76d
GodshyGaming <godshygaming@gmail.com>
2022-02-17 11:28:51 -0600

    Created basic console command system

--- Project_Chilidog/ConsoleEngine/ConsoleProgram.cs
+++ Project_Chilidog/ConsoleEngine/ConsoleProgram.cs
@@ -3,7 +3,7 @@ using Project_Chilidog.Goods;
 using Project_Chilidog.Merchants;
 using Project_Chilidog.TradeRoutes;
 
-namespace ConsoleEngine
+namespace Project_Chilidog.ConsoleEngine
 {
     /// <summary>
     /// Entrypoint for console version of program
@@ -13,6 +13,7 @@ namespace ConsoleEngine
     /// _globalSettlements: list of all settlements in the current worldspace
     /// _globalMerchants: list of all merchants in the current worldspace
     /// _globalTradeRoutes: list of all trade routes in the current worldspace
+    /// _commandList: dictionary to map all command strings to command methods
     /// </fields>
     /// 
     /// <methods>
@@ -23,20 +24,64 @@ namespace ConsoleEngine
     {
         #region Fields
 
+        #region Fields.GameObjects
+
         /// <summary>
         /// list of all settlements in the current worldspace
         /// </summary>
-        private List<ISettlement> _globalSettlements = new List<ISettlement>();
+        private static List<ISettlement> _globalSettlements = new List<ISettlement>();
 
         /// <summary>
         /// list of all merchants in the current worldspace
         /// </summary>
-        private List<IMerchant> _globalMerchants = new List<IMerchant>();
+        private static List<IMerchant> _globalMerchants = new List<IMerchant>();
 
         /// <summary>
         /// list of all trade routes in the current worldspace
         /// </summary>
-        private List<ITradeRoutes> _globalTradeRoutes = new List<ITradeRoutes>();
+        private static List<ITradeRoutes> _globalTradeRoutes = new List<ITradeRoutes>();
+
+        #endregion
+
+        #region Fields.Dictionaries
+
+        /// <summary>
+        /// dictionary to map all command strings to command methods
+        /// </summary>
+        private static Dictionary<
+            string,
+            Func<List<ISettlement>, List<IMerchant>, List<ITradeRoutes>, bool>
+            >
+            _commandList_SaveLoad = new Dictionary<
+            string,
+            Func<List<ISettlement>, List<IMerchant>, List<ITradeRoutes>, bool>
+            >()
+        {
+            { "Load", (
+                _globalSettlements,
+                _globalMerchants,
+                _globalTradeRoutes
+                )
+                    => ccSaveLoad.Load(
+                        _globalSettlements,
+                        _globalMerchants,
+                        _globalTradeRoutes
+                        ) 
+            },
+            { "Save", (
+                _globalSettlements,
+                _globalMerchants,
+                _globalTradeRoutes
+                )
+                    => ccSaveLoad.Save(
+                        _globalSettlements,
+                        _globalMerchants,
+                        _globalTradeRoutes
+                        )
+            },
+        };
+
+        #endregion
 
         #endregion
 
@@ -57,8 +102,29 @@ namespace ConsoleEngine
                 if (command == null)
                     throw new Exception("Error: Null Line");
                 else if (command == "")
-                    Console.WriteLine("You have to say something for me to know what you want");
+                    Console.WriteLine(
+                        "You have to say something for me to know what you want"
+                        );
                 Console.WriteLine(command);
+                foreach(var key in _commandList_SaveLoad.Keys)
+                {
+                    if (key == command)
+                    {
+                        if (_commandList_SaveLoad[key](
+                            _globalSettlements,
+                            _globalMerchants,
+                            _globalTradeRoutes
+                            ))
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Command failed to execute");
+                            break;
+                        }
+                    }
+                }
             }
         }
 
---
+++ Project_Chilidog/ConsoleEngine/ccSaveLoad.cs
@@ -0,0 +1,35 @@
+﻿namespace Project_Chilidog.ConsoleEngine
+{
+    public static class ccSaveLoad
+    {
+        #region Methods
+
+        /// <summary>
+        /// Saves the _globalX lists to a json file
+        /// </summary>
+        public static bool Save(
+            List<Settlements.ISettlement> GlobalSettlements,
+            List<Merchants.IMerchant> GlobalMerchants,
+            List<TradeRoutes.ITradeRoutes> GlobalTradeRoutes
+            )
+        {
+            Console.WriteLine("Not implemented");
+            return true;
+        }
+
+        /// <summary>
+        /// loads the _globalX lists from a json file
+        /// </summary>
+        public static bool Load(
+            List<Settlements.ISettlement> GlobalSettlements,
+            List<Merchants.IMerchant> GlobalMerchants,
+            List<TradeRoutes.ITradeRoutes> GlobalTradeRoutes
+            )
+        {
+            Console.WriteLine("Not implemented");
+            return true;
+        }
+
+        #endregion
+    }
+}
