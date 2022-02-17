30d0aa3e225059c76c841db51a5634002f73b707
GodshyGaming <godshygaming@gmail.com>
2022-02-17 09:41:35 -0600

    Absolute basic console functionality added

--- Project_Chilidog/ConsoleEngine/ConsoleProgram.cs
+++ Project_Chilidog/ConsoleEngine/ConsoleProgram.cs
@@ -1,4 +1,9 @@
-﻿namespace ConsoleEngine
+﻿using Project_Chilidog.Settlements;
+using Project_Chilidog.Goods;
+using Project_Chilidog.Merchants;
+using Project_Chilidog.TradeRoutes;
+
+namespace ConsoleEngine
 {
     /// <summary>
     /// Entrypoint for console version of program
@@ -15,7 +20,19 @@
         /// </summary>
         static void Main()
         {
+            Console.WriteLine("Welcome to Project Chilidog");
+            while (true)
+            {
+                string? command;
 
+                Console.Write("Chilidog 0.01a> ");
+                command = Console.ReadLine();
+                if (command == null)
+                    throw new Exception("Error: Null Line");
+                else if (command == "")
+                    Console.WriteLine("You have to say something for me to know what you want");
+                Console.WriteLine(command);
+            }
         }
     }
 }
