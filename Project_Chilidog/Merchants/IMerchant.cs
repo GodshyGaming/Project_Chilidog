fe553496a9f8965b3ff93981766f6033a6b68e1b
GodshyGaming <godshygaming@gmail.com>
2022-02-17 09:12:36 -0600

    Added basic fields to IMerchant

--- Project_Chilidog/Merchants/IMerchant.cs
+++ Project_Chilidog/Merchants/IMerchant.cs
@@ -9,5 +9,28 @@
     /// </summary>
     public interface IMerchant
     {
+        #region Fields
+
+        /// <summary>
+        /// Name of the Merchant
+        /// </summary>
+        string Name { get; set; }
+
+        /// <summary>
+        /// Description of the Merchant
+        /// </summary>
+        string Description { get; set; }
+
+        /// <summary>
+        /// Cash currently in Merchant's inventory
+        /// </summary>
+        decimal CashOnHand { get; set; }
+
+        /// <summary>
+        /// Goods currently in the Merchant's inventory
+        /// </summary>
+        List<Goods.IGood> InventoriedGoods { get; set; }
+
+        #endregion
     }
 }
