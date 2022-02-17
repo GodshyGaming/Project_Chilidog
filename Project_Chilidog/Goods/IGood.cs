38a736ff6d01e735030b48a3763890c5273d9db5
GodshyGaming <godshygaming@gmail.com>
2022-02-17 09:19:47 -0600

    Added basic fields to IGood

--- Project_Chilidog/Goods/IGood.cs
+++ Project_Chilidog/Goods/IGood.cs
@@ -7,7 +7,37 @@
     /// <summary>
     /// determines basic behavior for goods objects
     /// </summary>
+    /// 
+    /// <fields>
+    /// Name: Name of the good
+    /// Description: Description of the good
+    /// BaseValue: Basic value of the good, before calculating scarcity
+    /// Quantity: Quantity of good being represented
+    /// </fields>
     public interface IGood
     {
+        #region Fields
+
+        /// <summary>
+        /// Name of the good
+        /// </summary>
+        string Name { get; set; }
+
+        /// <summary>
+        /// Description of the good
+        /// </summary>
+        string Description { get; set; }
+
+        /// <summary>
+        /// Basic value of the good, before calculating scarcity
+        /// </summary>
+        decimal BaseValue { get; set; }
+
+        /// <summary>
+        /// Quantity of good being represented
+        /// </summary>
+        int Quantity { get; set; }
+
+        #endregion
     }
 }
