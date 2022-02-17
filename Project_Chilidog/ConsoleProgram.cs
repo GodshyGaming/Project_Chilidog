dea8ac5ceb132e70830098442472a705e48292cf
GodshyGaming <godshygaming@gmail.com>
2022-02-17 08:20:22 -0600

    Created base console entrypoint

---
+++ Project_Chilidog/ConsoleProgram.cs
@@ -0,0 +1,18 @@
+﻿/// <summary>
+/// Entrypoint for console version of program
+/// </summary>
+///
+/// <methods>
+/// Main();
+///     Entrypoint for console version of program
+/// </methods>
+class Program
+{
+    /// <summary>
+    /// Entrypoint for console version of program
+    /// </summary>
+    static void Main()
+    {
+
+    }
+}
